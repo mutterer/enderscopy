e78d474d4b646b2621e224b69d9ddc047ed53d28
crousseau <charly.rousseau@icm-institute.org>
2024-12-29 15:09:35 +0100

    REFACTORING: Fixes "self" not passed in methods and fixes style to PEP8

--- enderscope.py
+++ enderscope.py
@@ -1,12 +1,17 @@
 import glob
 import sys
 from time import sleep
-import serial
-from ipywidgets import widgets, Button, Layout, GridspecLayout, Output
-from IPython.display import display
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+
+from ipywidgets import widgets, Button, Layout, GridspecLayout, Output
+from IPython.display import display
+
+import serial
+
 
 G_CODES = {
     'absolute': 'G90',
@@ -16,23 +21,23 @@ G_CODES = {
     'current_position': 'M114'
 }
 DIRECTION_PREFIXES = {
-    "north": "Y",
-    "south": "Y-",
-    "east": "X",
-    "west": "X-",
-    "up": "Z",
-    "down": "Z-"
+    'north': 'Y',
+    'south': 'Y-',
+    'east': 'X',
+    'west': 'X-',
+    'up': 'Z',
+    'down': 'Z-'
 }
 
 class SerialUtils:
-
-    def serial_ports():
-        """ Lists serial port names
-        		from: https://stackoverflow.com/a/14224477
-            :raises EnvironmentError:
-                On unsupported or unknown platforms
-            :returns:
-                A list of the serial ports available on the system
+    @staticmethod
+    def serial_ports():  # TODO: check if this should be a function or a static method
+        """
+        Lists serial port names (from: https://stackoverflow.com/a/14224477)
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
         """
         if sys.platform.startswith('win'):
             ports = [f'COM{i + 1}' for i in range(256)]
@@ -59,8 +64,8 @@ class SerialDevice:
     def __init__(self, port, baud_rate, parity=serial.PARITY_NONE,
                  stop_bits=serial.STOPBITS_ONE, byte_size=serial.EIGHTBITS):
         self.serial = serial.Serial()
-        self.serial.port = port
 
+        self.serial.port = port
         self.serial.baudrate = baud_rate
         self.serial.parity = parity
         self.serial.stopbits = stop_bits
@@ -75,26 +80,25 @@ class SerialDevice:
             self.serial.read()
 
     def write_code(self, code):
-        if not code.endswith("\n"):
-            code += "\n"
-        self.serial.write(bytes(code, "utf-8"))
+        if not code.endswith('\n'):
+            code += '\n'
+        self.serial.write(bytes(code, 'utf-8'))
+
 
 class Stage(SerialDevice):
-    """
-    This is the 3 axis stage that moves the sample
-    """
+    """This is the 3 axis stage that moves the sample"""
 
     def __init__(self, port, baud_rate, homing=False, parity=serial.PARITY_NONE,
                  stop_bits=serial.STOPBITS_ONE, byte_size=serial.EIGHTBITS):
         super().__init__(port, baud_rate, parity, stop_bits, byte_size)
-        if homing==True:
+        if homing:
             self.home()
         
     def write_code(self, code, check_ok=True, debug=False):
         super().write_code(code)
         response = self.serial.readline().decode('utf-8')
         if check_ok:
-            while not response.startswith("ok"):
+            while not response.startswith('ok'):
                 if debug:
                     print (response.strip('\n'))
                 response = self.serial.readline().decode('utf-8')
@@ -105,47 +109,53 @@ class Stage(SerialDevice):
     def move_absolute(self, x, y, z=None, debug=False):
         """
         Moves the stage to the given coordinates
+
         :param x:
         :param y:
         :param z:
+        :param bool debug:
         :return:
         """
         self.set_absolute()
         if z is None:
-            code = f"G0 X {x} Y {y}"
+            code = f'G0 X {x} Y {y}'
         else:
-            code = f"G0 X {x} Y {y} Z {z}"
+            code = f'G0 X {x} Y {y} Z {z}'
         self.write_code(code, debug=debug)
 
     def move_position(self, p, debug=False):
         """
         Moves the stage to the given position
+
         :param p: position
+        :param bool debug:
         :return:
         """
         if p is not None:
-            self.set_absolute()
-            if len(p)<3 :
-                x,y = p
-                code = f"G0 X {x} Y {y}"
+            self.set_absolute()  # REFACTOR: use move_absolute
+            if len(p) < 3 :
+                x, y = p
+                code = f'G0 X {x} Y {y}'
             else:
-                x,y,z = p
-                code = f"G0 X {x} Y {y} Z {z}"
+                x, y, z = p
+                code = f'G0 X {x} Y {y} Z {z}'
             self.write_code(code, debug=debug)
             
     def move_relative(self, x, y, z=None, debug=False):
         """
         Moves the stage by given mm distance
+
         :param x:
         :param y:
         :param z:
+        :param bool debug:
         :return:
         """
         self.set_relative(debug=debug)
         if z is None:
-            code = f"G0 X {x} Y {y}"
+            code = f'G0 X {x} Y {y}'
         else:
-            code = f"G0 X {x} Y {y} Z {z}"
+            code = f'G0 X {x} Y {y} Z {z}'
         if debug:
             print(code)
         self.write_code(code, debug=debug)
@@ -153,41 +163,54 @@ class Stage(SerialDevice):
     def move_towards(self, direction, distance, debug=False):
         """
         Moves the stage in the given direction
+
         :param direction:
+        :param distance:
+        :param bool debug:
         :return:
         """
         self.set_relative()
         if direction.lower() in ('up', 'down'):
-            code = f"G0 {DIRECTION_PREFIXES[direction.lower()]}{distance}"
+            code = f'G0 {DIRECTION_PREFIXES[direction.lower()]}{distance}'
         else:
-            code = f"G0 {DIRECTION_PREFIXES[direction.lower()]}{distance}"
+            code = f'G0 {DIRECTION_PREFIXES[direction.lower()]}{distance}'
         self.write_code(code, debug=debug)
 
     def move_axis(self, axis, distance, debug=False):
         """
         Moves the stage along the given axis
+
+        :param axis:
         :param distance:
+        :param bool debug:
         :return:
         """
         self.set_relative()
-        code = f"G0 {axis.upper()}{distance}"
+        code = f'G0 {axis.upper()}{distance}'
         self.write_code(code, debug=debug)
 
-    def get_position(self, dict=False, debug=False):
+    def get_position(self, as_dict=False, debug=False):
+        """
+        Gets the current position of the stage
+
+        :param bool as_dict:
+        :param bool debug:
+        :return:
+        """
         self.flush_serial_buffer()
         response = self.write_code(G_CODES['current_position'],
                                    check_ok=False)
         if debug:
             print(response)
         ok = self.serial.readline()
-        if not ok.decode('utf-8').startswith("ok"):
-            print("Error reading stage position")
+        if not ok.decode('utf-8').startswith('ok'):
+            print('Error reading stage position')
             return
-        position = response.split(" Count")[0]
+        position = response.split(' Count')[0]
         parts = position.split()
-        positions = {part.split(":")[0]: float(part.split(":")[1]) for part in parts}
-        if dict==False:
-            order = ['X','Y', 'Z']
+        positions = {part.split(':')[0]: float(part.split(':')[1]) for part in parts}
+        if not as_dict:
+            order = 'XYZ'
             positions = tuple([positions[field] for field in order])
         return positions
 
@@ -203,50 +226,11 @@ class Stage(SerialDevice):
     def set_absolute(self, debug=False):
         self.write_code(G_CODES['absolute'], debug=debug)
 
-class Panel():
-    def create_button(self, description, bcolor):
-        b = Button(description=description, style=dict(button_color=bcolor), layout=Layout(height='auto', width='auto'))
-        b.on_click(self.on_button_clicked)
-        return b
-
-    def set_steps(self, xys,zs):
-        self.xys = xys
-        self.zs = zs
-        
-    def checkbox_changed(self,element):
-        if element['new'] == True:
-            self.recording = True
-            element['owner'].description = 'Recording...'
-        else:
-            self.recording = False
-            element['owner'].description = 'Record'
-        
-    def on_button_clicked(self, b):
-        if b.description == 'Home':
-            with self.output:
-                self.output.clear_output()
-                print ("homing...")
-            self.s.home()
-        elif b.description.startswith('P'):
-            m = int(b.description[-1])-1
-            if self.recording == True:
-                self.recorded_positions[m] = self.s.get_position()
-                b.style.button_color="#ffd6b9"
-            elif self.recorded_positions[m] is not None:
-                self.s.move_position(self.recorded_positions[m])
-        else:
-            self.s.move_towards(b.description,5)
-        with self.output:
-            self.output.clear_output()
-            print ("moving...")
-            self.s.finish_moves()
-            self.output.clear_output()
-            print (self.s.get_position(dict=True))
-            
-    def __init__(self, s):
+class Panel:
+    def __init__(self, stage):
         self.recording = False
-        self.recorded_positions = [None for i in range(6)] 
-        self.s = s
+        self.recorded_positions = [None] * 6
+        self.stage = stage
         grid = GridspecLayout(5, 12, height='auto', width='auto')
         grid[0:2, 0] = self.create_button('Up', 'paleturquoise')
         grid[2:, 0] = self.create_button('Down', 'paleturquoise')
@@ -258,21 +242,61 @@ class Panel():
         record_cb = widgets.Checkbox(value=False, description='Record', indent=False, layout=Layout(width='100px'))
         record_cb.observe(self.checkbox_changed, names='value')
         grid[0, 3:5] = record_cb
-        grid[1,3] = self.create_button('P1', 'lightgrey')
-        grid[1,4] = self.create_button('P2', 'lightgrey')
-        grid[2,3] = self.create_button('P3', 'lightgrey')
-        grid[2,4] = self.create_button('P4', 'lightgrey')
-        grid[3,3] = self.create_button('P5', 'lightgrey')
-        grid[3,4] = self.create_button('P6', 'lightgrey')
-        grid[4,3] = self.create_button('Save', 'pink')
-        grid[4,4] = self.create_button('Open', 'pink')
+        grid[1, 3] = self.create_button('P1', 'lightgrey')
+        grid[1, 4] = self.create_button('P2', 'lightgrey')
+        grid[2, 3] = self.create_button('P3', 'lightgrey')
+        grid[2, 4] = self.create_button('P4', 'lightgrey')
+        grid[3, 3] = self.create_button('P5', 'lightgrey')
+        grid[3, 4] = self.create_button('P6', 'lightgrey')
+        grid[4, 3] = self.create_button('Save', 'pink')
+        grid[4, 4] = self.create_button('Open', 'pink')
         self.xys = 5
         self.zs = 1
         self.grid = grid
         self.output = Output()
         display (grid, self.output)
 
-class Enderlights(SerialDevice):
+    def create_button(self, description, btn_color):
+        b = Button(description=description, style=dict(button_color=btn_color), layout=Layout(height='auto', width='auto'))
+        b.on_click(self.on_button_clicked)
+        return b
+
+    def set_steps(self, xys, zs):
+        self.xys = xys
+        self.zs = zs
+        
+    def checkbox_changed(self, element):
+        if element['new']:
+            self.recording = True
+            element['owner'].description = 'Recording...'
+        else:
+            self.recording = False
+            element['owner'].description = 'Record'
+        
+    def on_button_clicked(self, btn):
+        if btn.description == 'Home':
+            with self.output:
+                self.output.clear_output()
+                print ('homing...')
+            self.stage.home()
+        elif btn.description.startswith('P'):
+            m = int(btn.description[-1]) - 1
+            if self.recording:
+                self.recorded_positions[m] = self.stage.get_position()
+                btn.style.button_color= '#ffd6b9'
+            elif self.recorded_positions[m] is not None:
+                self.stage.move_position(self.recorded_positions[m])
+        else:
+            self.stage.move_towards(btn.description, 5)
+        with self.output:
+            self.output.clear_output()
+            print ('moving...')
+            self.stage.finish_moves()
+            self.output.clear_output()
+            print (self.stage.get_position(as_dict=True))
+            
+
+class EnderLights(SerialDevice):
     """
     An illumination device built from an Arduino board and a neopixels RGB leds ring
     """
@@ -284,77 +308,60 @@ class Enderlights(SerialDevice):
     def write_code(self, code, check_ok=True, debug=False):
         super().write_code(code)
         response = self.serial.readline().decode('utf-8')
-        if not response.startswith("ok"):
+        if not response.startswith('ok'):
             print (response.strip('\n'))
         return response
 
-    def shutter(self, s):
-        """
-        Opens or closes a virtual shutter
-        """
-        code = f"S0"
-        if s==True:
-            code = f"S1"
+    def shutter(self, s):  # TODO: replace name
+        """Opens or closes a virtual shutter"""
+        code = 'S1' if s else 'S0'
         self.write_code(code)
 
     def mode(self, value):
-        """
-        switches modes
-        """
-        code = f"M{value}"
+        """Switches modes"""
+        code = f'M{value}'
         self.write_code(code)
 
     def parameter(self, value):
-        """
-        switches modes
-        """
-        code = f"P{value}"
+        """Switches modes"""
+        code = f'P{value}'
         self.write_code(code)
 
     def red(self, value):
-        """
-        sets red level
-        """
-        code = f"R{value}"
+        """Sets red level"""
+        code = f'R{value}'
         self.write_code(code)
 
     def green(self, value):
-        """
-        sets green level
-        """
-        code = f"G{value}"
+        """Sets green level"""
+        code = f'G{value}'
         self.write_code(code)
 
     def blue(self, value):
-        """
-        sets green level
-        """
-        code = f"B{value}"
+        """Sets green level"""
+        code = f'B{value}'
         self.write_code(code)
 
-    def color(self, r,g,b):
-        """
-        sets rgb levels
-        """
+    def color(self, r, g, b):
+        """Sets RGB levels"""
         self.red(r)
         self.green(g)
         self.blue(b)
 
     def reset(self):
-        """
-        resets illuminator
-        """
+        """Resets illuminator"""
         self.shutter(False)
         self.mode(0)
-        self.write_code(f"MA65535\n")
+        self.write_code(f'MA65535\n')
         self.color(20,20,20)
 
+
 class ScanPatterns:
-    def plot_path(path = np.array([[0,0]]), labels=True, field = (10,10), title='Path preview'):
-        x=path[:, 0]
-        y=path[:, 1]
-        field = Rectangle((0,0),field[0],field[1])
-        rectangle = Rectangle((0,0), 200, 190,
+    def plot_path(self, path=np.array([[0,0]]), labels=True, field=(10, 10), title='Path preview'):
+        x = path[:, 0]
+        y = path[:, 1]
+        field = Rectangle((0, 0), field[0], field[1])
+        rectangle = Rectangle((0, 0), 200, 190,
                           edgecolor='green', facecolor='#00ff0010', linewidth=1)
         plt.gca().add_patch(rectangle)
         plt.plot(x,y, marker='x')
@@ -377,17 +384,17 @@ class ScanPatterns:
                              )
                 plt.gca().add_patch(f)
 
-    def raster(cols=4, rows=3):
+    def raster(self, cols=4, rows=3):
         return np.array(list((x,y) for y in range(rows) for x in range(cols)))
 
-    def snake(cols=4, rows=3):
+    def snake(self, cols=4, rows=3):
         return np.array(
             list((x,y)
                  for y in range(rows)
                  for x in range((cols-1)*(y%2),cols-(cols+1)*(y%2),((y+1)%2)-1*((y%2)))
                 ))
 
-    def random(num_points = 10, seed=1):
+    def random(self, num_points=10, seed=1):
         x_min, x_max = 0, 180  # Range for x values
         y_min, y_max = 0, 180  # Range for y values
         np.random.seed(seed)
@@ -395,16 +402,16 @@ class ScanPatterns:
             np.random.uniform(x_min, x_max, num_points),
             np.random.uniform(y_min, y_max, num_points)))
 
-    def spiral(num_points = 50):    
-        directions = np.array([[1,0],[0,1],[-1,0],[0,-1]])
+    def spiral(self, num_points=50):
+        directions = np.array([[1, 0], [0, 1], [-1, 0], [0, -1]])
         d = 0
         i = 1 
-        p = np.array([0,0])
+        p = np.array([0, 0])
         sp = np.array([p])
-        while len(sp)<num_points:
+        while len(sp) < num_points:
             for j in range(i):
-                p=p+directions[d]
+                p += directions[d]
                 sp = np.append(sp,[p], axis=0)
-            d = (d+1)%4
-            i = i + (d%2==0)            
+            d = (d+1) % 4
+            i += d%2 == 0
         return np.array(sp[:num_points])
